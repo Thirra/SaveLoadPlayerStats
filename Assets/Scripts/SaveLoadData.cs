32daf29a05ae38999c666f3d826b128421a33377
Kirsty Tschirpig <1004120@student.sae.edu.au>
2017-08-20 23:40:24 +1000

    Everything for the serialisation of player data, along with a test scene to show how it works

---
+++ Assets/Scripts/SaveLoadData.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml.Serialization;
+using System.IO;
+
+public class SaveLoadData : MonoBehaviour
+{
+    public static SaveLoadData Instance;
+    public PlayerStats playerStatistics;
+
+    public GameObject UIStuff;
+
+    private void Awake()
+    {
+        //Pseudo-singleton functionality 
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SaveData()
+    {
+        //Open a new XML file
+        XmlSerializer serializer = new XmlSerializer(typeof(PlayerStats));
+
+        //Creating a file over-writes anything that exists there - check the path first
+        FileStream stream = new FileStream(Application.dataPath + "/StreamingAssets/player_data.xml", FileMode.Create);
+
+        //This is where it's taking the information from the class and putting it into the xml file
+        serializer.Serialize(stream, playerStatistics);
+
+        stream.Close();
+    }
+
+    public void LoadData()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(PlayerStats));
+        FileStream stream = new FileStream(Application.dataPath + "/StreamingAssets/player_data.xml", FileMode.Open);
+        playerStatistics = (PlayerStats)serializer.Deserialize(stream);
+
+        stream.Close();
+
+        //UI things
+        int index = (int)playerStatistics.playerClass;
+        UIStuff.GetComponent<UIThings>().dropDown.value = index;
+        UIStuff.GetComponent<UIThings>().playerName.text = playerStatistics.playerName;
+        UIStuff.GetComponent<UIThings>().health.value = playerStatistics.health;
+        UIStuff.GetComponent<UIThings>().stamina.value = playerStatistics.stamina;
+        UIStuff.GetComponent<UIThings>().agility.value = playerStatistics.agility;
+    }
+}
+
+[System.Serializable]
+public class PlayerStats
+{
+    public string playerName;
+    public PlayerClass playerClass;
+    public int health;
+    public int stamina;
+    public int agility;
+}
+
+public enum PlayerClass
+{
+    Mage,
+    Warrior,
+    Warlock,
+    Paladin,
+    Priest
+}
+
---
+++ Assets/Scripts/UIThings.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIThings : MonoBehaviour
+{
+    public SaveLoadData saveLoadData;
+
+    public Dropdown dropDown;
+
+    //Don't really need these here
+    private List<string> classes;
+    private int index;
+
+    public InputField playerName;
+
+    public Slider health;
+    public Text healthText;
+
+    public Slider stamina;
+    public Text staminaText;
+
+    public Slider agility;
+    public Text agilityText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        saveLoadData = FindObjectOfType<SaveLoadData>();
+
+        PopulateList();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Added a numerical representation for slider
+        healthText.text = health.value.ToString();
+        staminaText.text = stamina.value.ToString();
+        agilityText.text = agility.value.ToString();
+    }
+
+    public void ifOnValueChanged()
+    {
+        string changedName = playerName.text;
+        saveLoadData.playerStatistics.playerName = changedName;
+    }
+
+    public void ddValueIsChanged()
+    {
+        index = dropDown.value;
+        saveLoadData.playerStatistics.playerClass = (PlayerClass)index;
+    }
+
+    public void healthOnValueChanged()
+    {
+        saveLoadData.playerStatistics.health = (int)health.value;
+    }
+
+    public void staminaOnValueChanged()
+    {
+        saveLoadData.playerStatistics.stamina = (int)stamina.value;
+    }
+
+    public void agilityOnValueChanged()
+    {
+        saveLoadData.playerStatistics.agility = (int)agility.value;
+    }
+
+    void PopulateList()
+    {
+        string[] enumNames = Enum.GetNames(typeof(PlayerClass));
+        classes = new List<string> (enumNames);
+        dropDown.AddOptions(classes);        
+    }
+}
